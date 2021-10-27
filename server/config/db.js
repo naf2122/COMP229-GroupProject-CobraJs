0a006cc5c24e9250027223eb434e742513089c63
nickcontini <ncontini@my.centennialcollege.ca>
2021-10-27 13:43:07 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://nick:y1U8EWZFyvXN9qfA@mongodbserber.f77bl.mongodb.net/books?retryWrites=true&w=majority",
+  "Secret": 'SomeSecret'
+};
